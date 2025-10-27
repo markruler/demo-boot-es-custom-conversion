7ae7951db7b2df56f0f97ee00825608586183866
Changsu Im <imcxsu@gmail.com>
2025-10-27 13:30:20 +0900

    Initial commit

---
+++ src/main/java/com/example/demobootescustomconversion/DemoBootEsCustomConversionApplication.java
@@ -0,0 +1,13 @@
+package com.example.demobootescustomconversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBootEsCustomConversionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoBootEsCustomConversionApplication.class, args);
+    }
+
+}
